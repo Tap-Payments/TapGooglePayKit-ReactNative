a223b828b00bb11b0cb09f7958a9c5b2f7090c0d
MahmoudAllam <m.allam@tap.company>
2025-09-16 14:35:35 +0300

    feat(Wrapper): update Native ios to support new arch

--- ios/TapGooglePayRn.h
+++ ios/TapGooglePayRn.h
@@ -1,12 +1,6 @@
 
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNTapGooglePayRnSpec.h"
-
-@interface TapGooglePayRn : NSObject <NativeTapGooglePayRnSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface TapGooglePayRn : NSObject <RCTBridgeModule>
-#endif
 
 @end
